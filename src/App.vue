4fa73a15f4580488cc11856c156b26c4832c52b1
Filiberto Reyes <filibertoreyes55@gmail.com>
2019-07-26 23:44:48 -0700

    added card background

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+body {
+    margin: 0;
+    text-align: center;
+
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,6 +22,7 @@
 }
 #nav {
   padding: 30px;
+  background-color: #374140;
   a {
     font-weight: bold;
     color: #2c3e50;
@@ -25,5 +30,17 @@
       color: #42b983;
     }
   }
+}
+.toolbar{
+  background-color: red;
+  width:100px;
+  height: 50px;
+}
+.triangle-right {
+	width: 0;
+	height: 0;
+	border-top: 25px solid transparent;
+	border-left: 50px solid #555;
+	border-bottom: 25px solid transparent;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home"> Current Screen: {{this.calculateScreen()}}
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="homeCard">
+      <!-- <div class="triangle-right"></div> -->
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+
+  },
+  methods:{
+    calculateScreen(){
+      var w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementsByTagName('body')[0],
+      x = w.innerWidth || e.clientWidth || g.clientWidth,
+      y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+      return (x + ' × ' + y);
+
+    }
+  },
+  computed: {
+    screen(){
+      var w = window,
+      d = document,
+      e = d.documentElement,
+      g = d.getElementsByTagName('body')[0],
+      x = w.innerWidth || e.clientWidth || g.clientWidth,
+      y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+      return (x + ' × ' + y);      
+    }
+  },
 }
 </script>
+
+<style lang="scss">
+.homeCard{
+    background-color: #E1E1E1;
+    background: url(../assets/png/bg2.png);
+    background-size: 1366px 768px;
+    width: 100%;
+    padding-bottom: 50%;
+
+
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-position: center; 
+}
+
+</style>
